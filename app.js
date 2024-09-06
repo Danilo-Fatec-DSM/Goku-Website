10329f59d11747dfbf148206c72040ab265e9eac
Danilo Santos <danilo.reis9@fatec.sp.gov.br>
2024-09-06 06:59:14 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-let buttonLogin = document.querySelector('#button-login')
+const buttonLogin = document.querySelector('#button-login')
+const buttonAccount = document.querySelector('#button-create-acount')
 
 function buttonLoginClicked() {
-    window.location.href = './form/form.html'
+    window.location.href = 'form_login/formLogin.html'
     console.log('clicked')
+}
+
+function buttonCreatAccountClicked() {
+    window.location.href = 'form_account/formAccount.html'
+    console.log('criar conta')
 }
