672d94c7667d559df9ca78a17f8b3557b9a7e2df
Danilo Santos <danilo.reis9@fatec.sp.gov.br>
2024-09-13 16:15:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const buttonAccount = document.querySelector('#button-create-acount')
 const buttonCart = document.querySelector('#button-shopping-cart')
 
 function buttonLoginClicked() {
-    window.location.href = '../form_login/formLogin.html'
+    window.location.href = 'form_login/formLogin.html'
     console.log('clicked')
 }
 
 function buttonCreatAccountClicked() {
-    window.location.href = '../form_account/formAccount.html'
+    window.location.href = 'form_account/formAccount.html'
     console.log('criar conta')
 }
 
 function buttonShoppingCart() {
-    window.location.href = '../shopping_cart/cart.html'
+    window.location.href = 'shopping_cart/cart.html'
 }
