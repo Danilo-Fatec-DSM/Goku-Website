c235286f5b94073c105098ac0c9c80f42d6a8cdd
Danilo Santos <danilosreis12@gmail.com>
2024-09-16 20:05:04 -0300

    create cart.js

---
+++ shopping_cart/cart.js
@@ -0,0 +1,10 @@
+const buttonLogin = document.querySelector('#button-login')
+const buttonAccount = document.querySelector('#button-create-acount')
+
+function buttonLoginClicked() {
+    window.location.href = '../form_login/formLogin.html'
+}
+
+function buttonCreatAccountClicked() {
+    window.location.href = '../form_account/formAccount.html'
+}
