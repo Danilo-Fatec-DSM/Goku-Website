c68711d84ca4ec4e6c71e713b8c0e329fea66eda
Danilo Santos <danilo.reis9@fatec.sp.gov.br>
2024-11-16 16:11:50 -0300

    Add files via upload

--- form_login/formLogin.js
+++ form_login/formLogin.js
@@ -1,17 +1,28 @@
-const users = JSON.parse(localStorage.getItem("users"));
+const users = JSON.parse(localStorage.getItem("users"))|| [];
 
 function login() {
-  let email = document.querySelector("#login").value;
-  let senha = document.querySelector("#password").value;
-  //laço for
+    let email = document.querySelector("#login").value;
+    let senha = document.querySelector("#password").value;
 
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].email == email && users[i].password == senha) {
-      alert(`${users[i].name}, logou`);
-      window.location.href = '../index.html'
+    if (users.length === 0) {
+        console.error("Nenhum usuário encontrado.");
+        alert("Nenhum usuário cadastrado.");
+        return;
     }
-  }
+
+    console.log(users[0]?.email, users[0]?.password);
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email === email && users[i].password === senha) {
+            alert(`${users[i].name}, logou`);
+            window.location.href = '../index.html';
+            return; // Para interromper o loop após login bem-sucedido
+        }
+    }
+
+    alert("Email ou senha incorretos.");
 }
+
 
 function garantirUsuarioDanilo() {
   // Obtém a lista de usuários do localStorage
